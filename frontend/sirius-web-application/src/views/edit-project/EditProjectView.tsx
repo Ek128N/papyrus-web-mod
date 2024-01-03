09f3ab0e17844c60f019d695d6f91b7867c45cce
Jessy Mallet <jessy.mallet@obeo.fr>
2024-01-03 18:11:33 +0100

    [118] Add profile application on Profile
    The user has now the capacity to apply a Profile on a Profile.
    
    Bug: https://github.com/PapyrusSirius/papyrus-web/issues/520
    Bug: https://gitlab.eclipse.org/eclipse/papyrus/org.eclipse.papyrus-web/-/issues/118
    Signed-off-by: Jessy Mallet <jessy.mallet@obeo.fr>

--- frontend/sirius-web-application/src/views/edit-project/EditProjectView.tsx
+++ frontend/sirius-web-application/src/views/edit-project/EditProjectView.tsx
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019, 2023 Obeo.
+ * Copyright (c) 2019, 2024 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -190,7 +190,8 @@ export const EditProjectView = () => {
           return (
             treeId.startsWith('explorer://') &&
             (item.kind === 'siriusComponents://semantic?domain=uml&entity=Model' ||
-              item.kind === 'siriusComponents://semantic?domain=uml&entity=Package')
+              item.kind === 'siriusComponents://semantic?domain=uml&entity=Package' ||
+              item.kind === 'siriusComponents://semantic?domain=uml&entity=Profile')
           );
         }}
         component={UMLModelTreeItemContextMenuContribution}
