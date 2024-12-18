a5dbdb6064942113079b2e81fa5a5044bfd72abf
Jerome Gout <jerome.gout@obeosoft.com>
2024-12-18 09:25:36 +0100

    GL-233 Add AQL service for Stereotype table
    Bug: https://gitlab.eclipse.org/eclipse/papyrus/org.eclipse.papyrus-web/-/issues/233
    Signed-off-by: Jerome Gout <jerome.gout@obeosoft.com>

--- backend/papyrus-web-application/src/main/java/org/eclipse/papyrus/web/application/representations/aqlservices/tables/TableService.java
+++ backend/papyrus-web-application/src/main/java/org/eclipse/papyrus/web/application/representations/aqlservices/tables/TableService.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2024 CEA LIST.
+ * Copyright (c) 2024, 2025 CEA LIST.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
@@ -21,22 +21,29 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
+import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.papyrus.uml.domain.services.properties.ILogger;
+import org.eclipse.papyrus.uml.domain.services.properties.PropertiesStereotypeApplicationServices;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.papyrus.web.application.tables.comment.UMLCommentTableRepresentationDescriptionBuilder;
+import org.eclipse.sirius.components.core.api.IEditingContext;
 import org.eclipse.sirius.components.core.api.IObjectService;
 import org.eclipse.sirius.components.emf.tables.CursorBasedNavigationServices;
 import org.eclipse.sirius.components.tables.ColumnFilter;
 import org.eclipse.sirius.components.tables.descriptions.PaginatedData;
 import org.eclipse.uml2.uml.Comment;
 import org.eclipse.uml2.uml.Element;
+import org.eclipse.uml2.uml.Package;
+import org.eclipse.uml2.uml.Stereotype;
+import org.eclipse.uml2.uml.Type;
 import org.eclipse.uml2.uml.UMLPackage;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 /**
@@ -46,18 +53,22 @@ import org.springframework.stereotype.Service;
 @Service
 public class TableService {
 
+    private final ILogger logger;
+
     private final IObjectService objectService;
 
     private final CursorBasedNavigationServices cursorBasedNavigationServices;
 
+    private final PropertiesStereotypeApplicationServices stereotypeApplicationServices;
+
     private final ObjectMapper objectMapper;
 
-    private final Logger logger = LoggerFactory.getLogger(TableService.class);
-
-    public TableService(IObjectService objectService, ObjectMapper objectMapper) {
+    public TableService(ILogger logger, IObjectService objectService, ObjectMapper objectMapper) {
+        this.logger = Objects.requireNonNull(logger);
         this.objectService = Objects.requireNonNull(objectService);
         this.objectMapper = Objects.requireNonNull(objectMapper);
         this.cursorBasedNavigationServices = new CursorBasedNavigationServices();
+        this.stereotypeApplicationServices = new PropertiesStereotypeApplicationServices(logger);
     }
 
     /**
@@ -122,7 +133,11 @@ public class TableService {
      * @return the alphabetic index starting to "A" equivalent to the given index.
      */
     public String alphabetic(Integer indexObject) {
-        var index = indexObject;
+        return this.alphabetic(indexObject, 0);
+    }
+
+    public String alphabetic(Integer indexObject, int offset) {
+        var index = indexObject + offset;
         if (index >= 0) {
             StringBuilder result = new StringBuilder();
             while (index >= 0) {
@@ -245,8 +260,157 @@ public class TableService {
             return this.objectMapper.readValue(columnFilter.value(), new TypeReference<>() {
             });
         } catch (JsonProcessingException exception) {
-            this.logger.warn(exception.getMessage(), exception);
+            this.logger.log(exception.getMessage(), ILogger.ILogLevel.WARNING);
         }
         return "";
+    }
+
+    /**
+     * Returns all types found underneath the given UML Package.
+     * @param umlPackage an UML Package
+     * @return the list of types owned by the given package
+     */
+    public List<Type> getOwnedTypes(Package umlPackage) {
+        return umlPackage.getOwnedTypes();
+    }
+
+    /**
+     * Return whether the given type contains the filters text in one of the properties of one stereotype applied to this type.
+     * @param type ab UML type
+     * @param editingContext the current editing context used to retrieve semantic objects
+     * @param globalFilter the global filter text
+     * @param columnFilters the list of column filters
+     * @return <code>true</code> if the given type contains the global filter text in one of the properties of one stereotype applied to this type and <code>false</code> otherwise.
+     * if no filters are given returns <code>true</code> as well.
+     */
+    public boolean containsFilterValue(Type type, IEditingContext editingContext, String globalFilter, List<ColumnFilter> columnFilters) {
+        boolean result = true;
+        if (globalFilter != null && !globalFilter.isBlank()) {
+            result = type.getStereotypeApplications().stream()
+                    .anyMatch(stereotypeApplication ->
+                            this.stereotypeApplicationServices.getAllFeatures(stereotypeApplication).stream()
+                                    .anyMatch(eStructuralFeature -> this.containsInValue(stereotypeApplication, eStructuralFeature, globalFilter)));
+        }
+        result = result && columnFilters.stream().allMatch(columnFilter -> {
+            boolean isMatching = true;
+            String columnFilterValue = this.getColumnFilterValue(columnFilter);
+            if (Objects.equals("stereotype", columnFilter.id())) {
+                isMatching = String.join("", this.getStereotypeApplicationLabels(type)).contains(columnFilterValue);
+            } else {
+                isMatching = type.getStereotypeApplications().stream()
+                        .anyMatch(stereotypeApplication -> {
+                            return this.objectService.getObject(editingContext, columnFilter.id()).stream()
+                                    .filter(EStructuralFeature.class::isInstance)
+                                    .map(EStructuralFeature.class::cast)
+                                    .map(eStructuralFeature -> this.getStereotypeCellValue(type, eStructuralFeature).toString().contains(columnFilterValue))
+                                    .findFirst()
+                                    .orElse(false);
+                        });
+            }
+            return isMatching;
+        });
+        return result;
+    }
+
+    private boolean containsInValue(EObject stereotypeApplication, EStructuralFeature feature, String globalFilter) {
+        return Optional.ofNullable(stereotypeApplication.eGet(feature)).stream()
+                .map(o -> o.toString().contains(globalFilter))
+                .findFirst()
+                .orElse(false);
+    }
+
+    /**
+     * Return the list of label of stereotype applications applied on the given element.
+     * @param self an UML element
+     * @return the list of label of stereotype applications applied on the given element separated by the given separator.
+     */
+    public List<String> getStereotypeApplicationLabels(Element self) {
+        return self.getStereotypeApplications().stream()
+                .map(EObject::eClass)
+                .map(this.objectService::getLabel)
+                .toList();
+    }
+
+    /**
+     * Return a joined String of the given list of String and the given separator.
+     * @param labels a list of String to join
+     * @param separator the separator placed in between two strings
+     * @return a joined String of the given list of String and the given separator.
+     */
+    public String join(List<String> labels, String separator) {
+        return String.join(separator, labels);
+    }
+
+    /**
+     * Return the list of all stereotypes applied to the given type.
+     * @param type an UML type
+     * @return the list of all stereotypes applied to the given type.
+     */
+    public List<Stereotype> getStereotypes(Type type) {
+        return  type.getApplicableStereotypes().stream()
+                .toList();
+    }
+
+    /**
+     * Return the list of all distinct stereotype classes applied to the given type.
+     * @param type an UML type
+     * @return the list of all distinct stereotype classes applied to the given type.
+     */
+    public List<EClass> getStereotypeClasses(Type type) {
+        return  type.getApplicableStereotypes().stream()
+                .map(Stereotype::getDefinition)
+                .toList();
+    }
+
+    /**
+     * Return the list of all valid structural features defined by the given EClass.
+     * @param eClass an EClass
+     * @return  the list of all valid structural features defined by the given EClass.
+     */
+    public List<EStructuralFeature> getAllFeatures(EClass eClass) {
+        return eClass.getEAllStructuralFeatures().stream()
+                .filter(this::isValidFeature)
+                .toList();
+    }
+
+    private boolean isValidFeature(EStructuralFeature feature) {
+        return !feature.getName().startsWith("base_") && !feature.isTransient() && !feature.isDerived()
+                && !this.isContainmentReference(feature);
+    }
+
+    private boolean isContainmentReference(EStructuralFeature feature) {
+        if (feature instanceof EReference reference) {
+            return reference.isContainment();
+        }
+        return false;
+    }
+
+    /**
+     * Returns the content of the cell between the given element (row) and the given feature (column).
+     * @param self a UML element
+     * @param columnFeature a structural feature of a stereotype application of the given element
+     * @return the textual value of the given structural feature or "N/A" if this feature is not available on the given element.
+     */
+    public Object getStereotypeCellValue(Element self, EStructuralFeature columnFeature) {
+        EObject stereotypeApplication = this.getStereotypeApplicationAppliedOn(self, columnFeature);
+        if (stereotypeApplication != null) {
+            return this.stereotypeApplicationServices.getStereotypeFeatureValue(stereotypeApplication, columnFeature);
+        } else {
+            return "N/A";
+        }
+    }
+
+    /**
+     * Given an Element and a feature, return the stereotype application applied on the Element defining the feature.
+     * @param self an Element
+     * @param columnFeature a feature
+     * @return the stereotype application applied on the given Element defining the given feature or <code>null</code> if the feature is not belonging to any stereotype application of the element.
+     */
+    private EObject getStereotypeApplicationAppliedOn(Element self, EStructuralFeature columnFeature) {
+        EClass stereotypeApplicationEClass = columnFeature.getEContainingClass();
+        return self.getStereotypeApplications().stream()
+                .filter(eObject -> Objects.equals(eObject.eClass(), stereotypeApplicationEClass))
+                .findFirst()
+                .orElse(null);
     }
 }
